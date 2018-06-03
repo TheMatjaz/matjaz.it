6daa52a50e17dc393d24f51b5a5e026e2f3450e7
Matjaž <dev@matjaz.it>
2018-06-03 16:33:30 +0200

    Implement state storage into query string

--- static/matrix/sorting.js
+++ static/matrix/sorting.js
@@ -2,7 +2,27 @@ var g_output_list_element = document.getElementById("sorted_list");
 var g_input_fields = document.getElementsByClassName("cell");
 var g_cells = {};
 
-prepare_event_listeners();
+initialize();
+
+function initialize() {
+    /*
+    load_input_field_contents_from_query_string();
+    refresh_cells_dict_from_input_fields();
+    refresh_query_string_from_cells_dict();
+    refresh_output_list_from_cells_dict();
+    */
+    prepare_event_listeners();
+}
+
+function refresh_cells_dict_from_input_fields() {
+    // The browser caches which were the last values written in the
+    // input fields. Read the values in the fields and load them to
+    // g_cells.
+    Object.values(g_input_fields).forEach(function(input_field) {
+        var cell = input_field_to_cell(input_field);
+        g_cells[cell.id] = cell;
+    });
+}
 
 function prepare_event_listeners() {
     Object.values(g_input_fields).forEach(function(input_field) {
@@ -14,7 +34,8 @@ function prepare_event_listeners() {
 function input_field_event(input_field) {
     var cell = input_field_to_cell(input_field);
     g_cells[cell.id] = cell;
-    update_output_list(g_output_list_element, g_cells);
+    refresh_output_list_from_cells_dict();
+    refresh_query_string_from_cells_dict();
 }
 
 function input_field_to_cell(input_field) {
@@ -32,8 +53,8 @@ function clean_string(string) {
     return string.trim();
 }
 
-function update_output_list(output_list_element, cells) {
-    output_list_element.innerHTML = cells_to_sorted_html_list(cells);
+function refresh_output_list_from_cells_dict() {
+    g_output_list_element.innerHTML = cells_to_sorted_html_list(g_cells);
 }
 
 function cells_to_sorted_html_list(cells) {
@@ -72,14 +93,22 @@ function cell_to_html_list_element(cell) {
            + "</li>";
 }
 
+function refresh_query_string_from_cells_dict() {
+    Object.values(g_cells).forEach(function(cell) {
+        save_cell_to_query_string(cell);
+    });
+}
+
 function save_cell_to_query_string(cell) {
-    if (cell.is_valid()) {
-        window.location.search = update_query_string_field(
+    var new_query_string;
+    if (cell != null && cell.is_valid()) {
+        new_query_string = update_query_string_field(
                 window.location.search, cell.id, cell.value);
     } else {
-        window.location.search = update_query_string_field(
+        new_query_string = update_query_string_field(
                 window.location.search, cell.id, null);
     }
+    rewrite_query_string_in_url(new_query_string);
 }
 
 function update_query_string_field(uri, key, value) {
@@ -88,34 +117,43 @@ function update_query_string_field(uri, key, value) {
     var separator = uri.indexOf('?') !== -1 ? "&" : "?";
     var new_uri;
     if (uri.match(re)) {
-        if (value === null) {
-            new_uri = uri.replace(re, '$1' + '$2'); // Remove key and value
+        if (value == null) {
+            new_uri = uri.replace(re, ''); // Remove key and value
         } else {
             new_uri = uri.replace(re, '$1' + key + "=" + value + '$2');
         }
     } else {
-        new_uri = uri + separator + key + "=" + value;
+        if (value != null) {
+            new_uri = uri + separator + key + "=" + value;
+        } else {
+            new_uri = uri;
+        }
     }
     return new_uri;
 }
 
-function load_cells_from_query_string() {
+function rewrite_query_string_in_url(new_query_string) {
+    // https://stackoverflow.com/a/19279428/5292928 in the comments
+    const new_url = new URL(window.location.href);
+    new_url.search = new_query_string;
+    window.history.replaceState({ path: new_url.href }, '', new_url.href);
+}
+
+function load_input_field_contents_from_query_string() {
     Object.values(g_input_fields).forEach(function(input_field) {
-        var cell_value = read_query_string_field(window.location.search, input_field.name);
-        if (cell_value !== null) {
+        var cell_value = read_query_string_field(
+            window.location.search, input_field.name);
+        if (cell_value != null) {
             input_field.value = cell_value;
-            var cell = input_field_to_cell(input_field);
-            g_cells[cell.id] = cell;
         }
     });
-    update_output_list(g_output_list_element, g_cells);
 }
 
 function read_query_string_field(uri, key) {
     // https://stackoverflow.com/a/6021027/5292928
     var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
     var matched_values = uri.match(re);
-    if (matched_values === null) {
+    if (matched_values == null) {
         return null;
     } else {
         return matched_values[1];
